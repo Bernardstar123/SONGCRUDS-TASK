f92c0ccae7a6045946c3689dd423d87f2755084e
unknown <bernardsamson2003@yahoo.com>
2022-10-29 01:10:04 +0100

    Week 5 Python Task: Djangp CRUD by Ben

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,19 @@
+from tkinter.filedialog import asksaveasfile
+from tkinter.messagebox import YES
 from django.db import models
 
 # Create your models here.
+class Artiste (models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    age=models.CharField(max_lenght=3)
+
+class Song (models.Model):
+    title=("Fire on the mountain")
+    date_released= 2007
+    likes = YES
+    artiste_id = ("Asa")
+
+class Lyrics (models.Model):
+    content = ("")
+    song_id = ("")
